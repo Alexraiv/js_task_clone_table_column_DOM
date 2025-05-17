ff3237e2e64fd8f8e49937cd19df8606fd0d0c96
Oleksandr Voronkin <94705289+Alexraiv@users.noreply.github.com>
2025-05-17 18:02:15 +0300

    fix1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,11 @@
 // write your code here
 const table = document.querySelector('table');
 const rows = [...table.querySelectorAll('tr')];
-const COPY_COLUMN = 1;
 
 rows.forEach((row, i) => {
   const section = row.closest('thead, tfoot') ? 'th' : 'td';
   const cell = document.createElement(section);
 
-  cell.textContent = rows[i].children[COPY_COLUMN]?.textContent || '';
+  cell.textContent = rows[i].children[1]?.textContent || '';
   row.insertBefore(cell, row.lastElementChild);
 });
